eda0eb2b1c3d1caf059ea809397d4c31ab327b7c
Moritz Moeller <mm@mxs.de>
2019-09-01 20:53:58 +0200

    ..

---
+++ asd.ts
@@ -0,0 +1,29 @@
+// @TODO: react-mo-core?
+export function onWillUnmount(self: React.Component, callback: () => void) {
+  const prev = self.componentWillUnmount ? self.componentWillUnmount.bind(self) : undefined;
+  self.componentWillUnmount = () => {
+    if (prev) prev();
+    callback();
+  };
+}
+
+// @TODO: react-mo-core?
+export function releaseOnWillUnmount(self: React.Component, sub: Releaseable) {
+  onWillUnmount(self, () => sub.release());
+}
+
+// @TODO: react-mo-core?
+export function SafeSetState<S>(self: React.Component<any, S>) {
+  let mounted = true;
+  onWillUnmount(self, () => {
+    mounted = false;
+  });
+  return (arg: Pick<S, keyof S>) => {
+    if (mounted) {
+      self.setState(arg);
+    }
+  };
+}
+
+
+
